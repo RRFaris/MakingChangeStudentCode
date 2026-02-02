f6ebf7a4f4a3f7e2d43a5a61ed13ba323e3f0982
Ryan-dev12 <ryan.faris007@gmail.com>
2026-02-01 22:26:25 -0800

    Finished V2

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -37,16 +37,21 @@ public class MakingChange {
     public static long countWays(int target, int[] coins) {
         long[][] tabs = new long[target + 1][coins.length];
 
+        // Fill in the first column with 1's because that is a "base case" in this scenario
         for (int i = 0; i < coins.length; i++) {
             tabs[0][i] = 1;
         }
 
         for (int i = 1; i < target + 1; i++) {
             for (int j = 0; j < coins.length; j++) {
+                // Prevents out of bounds
                 if (i - coins[j] >= 0) {
+                    // Add the included case
                     tabs[i][j] += tabs[i - coins[j]][j];
                 }
+                // Prevents out of bounds
                 if (j - 1 >= 0) {
+                    // Add the excluded case
                     tabs[i][j] += tabs[i][j-1];
                 }
             }

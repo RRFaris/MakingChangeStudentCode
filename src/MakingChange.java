7f44b608656753c8b6dbdc9b35ed4d8a1f0ec0b4
Ryan-dev12 <ryan.faris007@gmail.com>
2026-02-01 17:19:02 -0800

    Finished a working version of tabulation

--- src/MakingChange.java
+++ src/MakingChange.java
@@ -7,29 +7,51 @@
  */
 
 public class MakingChange {
+//    public static long countWays(int target, int[] coins) {
+//        long[][] memo = new long[target + 1][coins.length];
+//
+//        return count(target, coins.length-1, coins, memo);
+//    }
+//
+//    // Recursive function
+//    public static long count(int sum, int index, int[] coins, long[][] memo) {
+//        // Base cases
+//        if (sum == 0)
+//            return 1;
+//
+//        if (index < 0)
+//            return 0;
+//
+//        if (sum < 0)
+//            return 0;
+//
+//        if (memo[sum][index] == 0) {
+//            memo[sum][index] = count(sum - coins[index], index, coins, memo) + count(sum, index - 1, coins, memo);
+//            return memo[sum][index];
+//        }
+//
+//        return memo[sum][index];
+//    }
+
+
     public static long countWays(int target, int[] coins) {
-        long[][] memo = new long[target + 1][coins.length];
+        long[][] tabs = new long[target + 1][coins.length];
 
-        return count(target, coins.length-1, coins, memo);
-    }
-
-    // Recursive function
-    public static long count(int sum, int index, int[] coins, long[][] memo) {
-        // Base cases
-        if (sum == 0)
-            return 1;
-
-        if (index < 0)
-            return 0;
-
-        if (sum < 0)
-            return 0;
-
-        if (memo[sum][index] == 0) {
-            memo[sum][index] = count(sum - coins[index], index, coins, memo) + count(sum, index - 1, coins, memo);
-            return memo[sum][index];
+        for (int i = 0; i < coins.length; i++) {
+            tabs[0][i] = 1;
         }
 
-        return memo[sum][index];
+        for (int i = 1; i < target + 1; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    tabs[i][j] += tabs[i - coins[j]][j];
+                }
+                if (j - 1 >= 0) {
+                    tabs[i][j] += tabs[i][j-1];
+                }
+            }
+        }
+
+        return tabs[target][coins.length - 1];
     }
 }
